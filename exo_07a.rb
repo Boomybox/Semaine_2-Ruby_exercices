195d6d88136e53633b2b9a5e07980e0a7c7082fd
Cassandra BIJEARD <52466193+Boomybox@users.noreply.github.com>
2019-07-09 22:05:05 +0200

    Add exo_07a to repo

---
+++ exo_07a.rb
@@ -0,0 +1,8 @@
+#Peux-tu me dire ce que fais gets.chomp dans le programme qui suit ?
+
+
+puts "Bonjour, c'est quoi ton blase ?"
+user_name = gets.chomp
+puts user_name
+
+# dans ce cas, gets.chomp permet de rajouter un champ actif pour demander à l'utilisateur d'entrer son nom qui sera stocké dans une variable qu'on pourra réutiliser ensuite. Il s'agit d'une variable de type String.
