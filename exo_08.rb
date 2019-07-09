9555dbd5eea64b0c714a767bf5fc2fa2ac01d135
Cassandra BIJEARD <52466193+Boomybox@users.noreply.github.com>
2019-07-09 22:15:51 +0200

    Add exo_08 to repo

---
+++ exo_08.rb
@@ -0,0 +1,7 @@
+# Enoncé : Écris un programme exo_08.rb qui demande le prénom de l'utilisateur, et qui salue l'utilisateur avec "Bonjour, prénom !"
+
+puts "Quel est ton prénom ?"
+
+first_name = gets.chomp
+
+puts "Bonjour, #{first_name} !"
