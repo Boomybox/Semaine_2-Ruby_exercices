dff21f0a8249e78fc0c164070d8ddc852c588c64
Cassandra BIJEARD <52466193+Boomybox@users.noreply.github.com>
2019-07-09 23:06:07 +0200

    Mise à jour énoncé

--- exo_18.rb
+++ exo_18.rb
@@ -1,4 +1,6 @@
-=begin Écris un programme exo_18.rb qui va créer une liste de 50 faux emails et les stocker dans une array. Voici le format que devront avoir les faux emails :
+=begin 
+Enoncé: Écris un programme exo_18.rb qui va créer une liste de 50 faux emails et les stocker dans une array. 
+Voici le format que devront avoir les faux emails :
 "jean.dupont.01@email.fr"
 "jean.dupont.02@email.fr"
 etc..
