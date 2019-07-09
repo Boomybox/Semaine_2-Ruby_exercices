02341e697eb174a2d89dc6be177cf986c869b260
Cassandra BIJEARD <52466193+Boomybox@users.noreply.github.com>
2019-07-09 22:13:22 +0200

    Add exo_07b & exo_07c to repo

---
+++ exo_07b.rb
@@ -0,0 +1,6 @@
+puts "Bonjour, c'est quoi ton blase ?"
+print "> "
+user_name = gets.chomp
+puts user_name
+
+# Dans cet exemple, une flèche apparait devant le champ permettant à l'utilisateur d'entrer du texte grâce à la commande print ">", qui contrairement à puts ne ramène pas à la ligne. 
---
+++ exo_07c.rb
@@ -0,0 +1,4 @@
+user_name = gets.chomp
+puts user_name
+
+#dans cette exemple, le champ permettant à l'utilisateur d'entrée du texte n'est pas précisé, on ne sait donc pas ce qu'il faut rentrer et l'utilisateur peut très bien entrer un mot au hasard comme "chien" au lieu de rentrer son nom.
