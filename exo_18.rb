7918e0d64656fcb6da64f345a36aebd4116b3cee
Cassandra BIJEARD <52466193+Boomybox@users.noreply.github.com>
2019-07-09 23:04:09 +0200

    Add exo_18 to repo

---
+++ exo_18.rb
@@ -0,0 +1,27 @@
+=begin Écris un programme exo_18.rb qui va créer une liste de 50 faux emails et les stocker dans une array. Voici le format que devront avoir les faux emails :
+"jean.dupont.01@email.fr"
+"jean.dupont.02@email.fr"
+etc..
+=end 
+
+nb = 1
+
+emails = []
+
+while nb <= 50
+
+	if nb < 10
+
+		emails << "jean.dupont.0#{nb}@email.fr"
+
+	else
+
+		emails << "jean.dupont.#{nb}@email.fr"
+		
+	end
+
+	nb += 1
+	
+end
+
+puts emails
