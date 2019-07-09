37ad74ced001e2ca9153e05dafa6964549c26b2a
Cassandra BIJEARD <52466193+Boomybox@users.noreply.github.com>
2019-07-09 21:36:33 +0200

    Add exo_04 to repo

---
+++ exo_04.rb
@@ -0,0 +1,14 @@
+=begin 
+Enoncé : Créé un programme exo_04.rb avec les lignes suivantes :
+
+puts "Salut, ça farte ?
+
+Lance le programme. Que se passe-t-il ? Pourquoi ? 
+
+=end
+
+puts "Salut, ça farte ?
+
+
+-> string n'est pas fermé donc pas pris en compte par le terminal et produit une erreur. 
+Pour que cela marche il faut fermer les guillements à la fin de la phrase.
