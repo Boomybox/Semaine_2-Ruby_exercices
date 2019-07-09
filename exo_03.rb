4fb5357b273d1f393b8c1145a0033b1bff04ce88
Cassandra BIJEARD <52466193+Boomybox@users.noreply.github.com>
2019-07-09 21:30:04 +0200

    Add exo_03 to repo

---
+++ exo_03.rb
@@ -0,0 +1,7 @@
+puts "Bonjour, monde !"
+#puts "Et avec une voix sexy, ça donne : Bonjour, monde !"
+
+=begin
+rajouter un "#" permet de faire un commentaire, la seconde ligne n'est donc plus
+exécutée par le terminal et seule la première ligne apparait à l'oeil de l'utilisateur.
+=end
